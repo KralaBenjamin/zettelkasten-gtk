567a53c7be2633575063fa8f2c73bd815b2b6797
snowparrot <snowparrot@live.com>
2021-04-11 16:34:08 +0200

    refactor main und MainWindow

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,31 +1,72 @@
 from gi.repository import Gtk
 from gi.repository import Granite
 from SearchContainer import SearchContainer
+from SearchResultView import SearchResultView
+from ZettelWindow import ZettelWindow
+
 
 
 class MainWindow(Gtk.Window):
 
-    def __init__(self) -> None:
+    def __init__(self, zdata) -> None:
         super().__init__()
+        self.zdata = zdata
+
         self.create_layout()
 
-
+        self.connect("destroy", Gtk.main_quit)
+        self.sc.search_button.connect("clicked", self.on_search_button_factory())
+        self.create_new_zettel_button.connect("clicked", 
+                self.on_clicked_create_new_zettel_button_factory())
 
     def create_layout(self):
         self.header_bar = Gtk.HeaderBar()
         self.header_bar.set_show_close_button(True)
         self.header_bar.props.title = "Zettelkasten"
-        self.header_bar_button = \
+        self.create_new_zettel_button = \
                 Gtk.Button.new_from_icon_name('document-new', Gtk.IconSize.LARGE_TOOLBAR)
 
         self.set_titlebar(self.header_bar)
-        self.header_bar.pack_start(self.header_bar_button)
+        self.header_bar.pack_start(self.create_new_zettel_button)
 
         self.set_default_size(500, 500)
 
         self.sc = SearchContainer()
         self.add(self.sc)
 
+    def on_search_button_factory(self):
+        def on_search_button(button):
+            ## Todo: Suchtreffer markieren
+            ## Todo: Ordnung der Ergebnisse verbessern
+
+            self.sc.clear_search_view()
+
+            search_term = self.sc.search_entry.get_text()
+            results = self.zdata.search(search_term)
+            
+            if len(results) == 0:
+                search_label = \
+                    Gtk.Label(label=f"{search_term} hat keine Suchtreffer ergeben")
+            else: 
+                search_label = \
+                    Gtk.Label(label=f"Suche: {search_term} ergab {len(results)} Suchergebnisse")
+            
+            self.sc.add_view_into_search_view(search_label)
+            search_label.show()
+
+            for result in results:
+                new_zettel_view = SearchResultView(result)
+                new_zettel_view.set_halign(Gtk.Align.CENTER)
+                self.sc.add_view_into_search_view(new_zettel_view)
+                new_zettel_view.show()
+        return on_search_button
+
+    def on_clicked_create_new_zettel_button_factory(self):
+        def on_clicked_create_new_zettel_button(button):
+            create_window = ZettelWindow(self.zdata)
+            create_window.show_all()
+            create_window.connect("destroy", Gtk.close)
+        return on_clicked_create_new_zettel_button
 
 
 if __name__ == "__main__":
--- src/main.py
+++ src/main.py
@@ -6,53 +6,15 @@ import argparse
 gi.require_version('Gtk', '3.0')
 gi.require_version('Granite', '1.0')
 from gi.repository import Gtk, Gio, Gdk, Granite, GObject
-from SearchWindow import SearchWindow
 from ZettelDataService import ZettelDataService
-from SearchResultView import SearchResultView
 from MainWindow import MainWindow
-from ZettelWindow import ZettelWindow
 
 
 
 zuri = "/home/snowparrot/NextCloud/Zettelkasten"
 zData = ZettelDataService(zuri)
 
-window = MainWindow() ## auf MainWindow ändern
+window = MainWindow(zData) 
 window.show_all()
-window.connect("destroy", Gtk.main_quit)
-def on_search_button(button):
-    ## Todo: Suchtreffer markieren
-    ## Todo: Ordnung der Ergebnisse verbessern
-
-    window.sc.clear_search_view()
-
-    search_term = window.sc.search_entry.get_text()
-    results = zData.search(search_term)
-    
-    if len(results) == 0:
-        search_label = \
-            Gtk.Label(label=f"{search_term} hat keine Suchtreffer ergeben")
-    else: 
-        search_label = \
-            Gtk.Label(label=f"Suche: {search_term} ergab {len(results)} Suchergebnisse")
-    
-    window.sc.add_view_into_search_view(search_label)
-    search_label.show()
-
-    for result in results:
-        new_zettel_view = SearchResultView(result)
-        new_zettel_view.set_halign(Gtk.Align.CENTER)
-        window.sc.add_view_into_search_view(new_zettel_view)
-        new_zettel_view.show()
-
-
-def on_create_button(button):
-    create_window = ZettelWindow(zData)
-    create_window.show_all()
-    create_window.connect("destroy", Gtk.close)
-    
-
-window.sc.search_button.connect("clicked", on_search_button)
-window.header_bar_button.connect("clicked", on_create_button)
 
 Gtk.main()
