ea2e678347ce43a7f86e1bd08b1c39b2bd351b96
snowparrot <snowparrot@live.com>
2021-04-11 16:40:14 +0200

    lösche main.start

--- src/main.py
+++ src/main.py
@@ -1,14 +1,10 @@
 
 import gi
-import locale
-import gettext
-import argparse
 gi.require_version('Gtk', '3.0')
 gi.require_version('Granite', '1.0')
 from gi.repository import Gtk, Gio, Gdk, Granite, GObject
 from ZettelDataService import ZettelDataService
 from MainWindow import MainWindow
-
 
 
 zuri = "/home/snowparrot/NextCloud/Zettelkasten"
