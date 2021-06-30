551ca355b2e9fde0f2843f3b842ea3384fba9683
snowparrot <snowparrot@live.com>
2021-06-30 21:14:40 +0200

    dict für die Methode

--- src/ZettelSortingMethods.py
+++ src/ZettelSortingMethods.py
@@ -1,21 +1,17 @@
 class ZettelSortingMethods:
-
     @staticmethod
     def sorted_zettel_date_old_to_last(zettel_list):
         return sorted(zettel_list,
                       key=lambda zettel: int(zettel.file_name[:-3]))
-
     @staticmethod
     def sorted_zettel_date_last_to_old(zettel_list):
         return sorted(zettel_list,
                       key=lambda zettel: int(zettel.file_name[:-3]),
                       reverse=True)
-
     @staticmethod
     def sorted_zettel_name_a_to_z (zettel_list):
         return sorted(zettel_list,
               key=lambda zettel: zettel.title)
-
     @staticmethod
     def sorted_zettel_name_z_to_a (zettel_list):
         return sorted(zettel_list,
@@ -23,20 +19,25 @@ class ZettelSortingMethods:
                       reverse=True)
 
 
-    list_all_sorting_methods = [
+list_all_sorting_methods = [
         {
             "string-id": "sort_zettel_date_old_to_last",
-            "display-string": "Datum: Alte nach Neu",
-            "sorting-method": sort_zettel_date_old_to_last
+            "display-string": "Datum: Alt nach Neu",
+            "sorting-method": ZettelSortingMethods.sorted_zettel_date_old_to_last
         },
         {
             "string-id": "sort_zettel_date_last_to_old",
             "display-string": "Datum: Neu nach Alt",
-            "sorting-method": sort_zettel_date_last_to_old
+            "sorting-method": ZettelSortingMethods.sorted_zettel_date_last_to_old
         },
         {
             "string-id": "sort_zettel_name_a_to_z",
             "display-string": "Titel: A nach Z",
-            "sorting-method": sort_zettel_name_a_to_z
+            "sorting-method": ZettelSortingMethods.sorted_zettel_name_a_to_z
+        },
+        {
+            "string-id": "sorted_zettel_name_z_to_a",
+            "display-string": "Titel: Z nach A",
+            "sorting-method": ZettelSortingMethods.sorted_zettel_name_z_to_a
         }
     ]
