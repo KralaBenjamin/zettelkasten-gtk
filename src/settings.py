e58f5338e352bae48f1a8cfbb76d5c756579ec67
Benjamin Krala <kralabenjamin@gmail.com>
2022-05-05 22:12:02 +0200

    verbesserte config

--- src/settings.py
+++ src/settings.py
@@ -1,21 +1,25 @@
 import json
+import os
 
 
 
 class Settings:
-    LOCATION = "settings.json"
+    LOCATION = f"{os.environ['HOME']}/.config/zettelkasten-gtk/settings.json"
 
 
     def __init__(self):
         """
             Init Settings
         """
-
-        self.settings_dict = json.load(open(LOCATION, "r+"))
+        try:
+            self.settings_dict = json.load(open(LOCATION))
+        except:
+            self.settings_dict = None
 
     def is_settings_zk_location_avalaible(self):
         
-        return "zk_locations" in self.settings_dict and \
+        return bool(self.settings_dict) and \
+            "zk_locations" in self.settings_dict and \
             isinstance(self.settings_dict["zk_locations"], list) and \
             len(self.settings_dict["zk_locations"]) > 0
 
@@ -23,9 +27,22 @@ class Settings:
         return self.settings_dict["zk_locations"]
 
 
-if __name__ == "__main__":
-    LOCATION = "settings.json"
+    def create_new_settings(self):
+        pass
 
+
+    def get_window_properties(self):
+        pass
+
+
+    def set_window_properties(self):
+        pass
+
+
+    #position, Größe abspeichern
+
+if __name__ == "__main__":
+    LOCATION = f"{os.environ['HOME']}/.config/zettelkasten-gtk/settings.json" 
     seti = Settings()
     print(seti.is_settings_zk_location_avalaible())
     json.dump({}, open(LOCATION, "w+"))
