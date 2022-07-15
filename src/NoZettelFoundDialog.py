108ff9685a442013f190df6a2c53ea3b4f287dfe
Benjamin Krala <kralabenjamin@gmail.com>
2022-07-15 19:52:43 +0200

    füge Signale hinzu

--- src/NoZettelFoundDialog.py
+++ src/NoZettelFoundDialog.py
@@ -2,19 +2,64 @@ import gi
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+from gi.repository import GObject
 
 
 class NoZettelFoundDialog(Gtk.MessageDialog):
     def __init__(self):
         super().__init__(title="Kein Ordner mit Zettel gefunden", flags=0)
+
         self.add_buttons(
             Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK
         )
         self.format_secondary_markup("Um die Zettel durchsuchen zu können, braucht es ein Ordner.")
-
         self.set_default_size(200, 150)
+
+        GObject.signal_new(
+            "test1",
+            self,
+            GObject.SignalFlags.RUN_FIRST,
+            GObject.TYPE_PYOBJECT, 
+            (GObject.TYPE_PYOBJECT,)
+        )
+
+
+        GObject.signal_new(
+            "test2",
+            self,
+            GObject.SignalFlags.RUN_FIRST,
+            GObject.TYPE_PYOBJECT, 
+            (GObject.TYPE_PYOBJECT,)
+
+        )
+
+        GObject.signal_new(
+            "cancel_button_clicked",
+            self,
+            GObject.SignalFlags.RUN_FIRST,
+            GObject.TYPE_PYOBJECT, 
+            (GObject.TYPE_PYOBJECT,)
+        )
+
+        GObject.signal_new(
+            "zettelkasten_dir_chosen",
+            self,
+            GObject.SignalFlags.RUN_FIRST,
+            GObject.TYPE_PYOBJECT, 
+            (GObject.TYPE_PYOBJECT,)
+        )
+
+
+        self.emit("test1", "test")
         cancel_button = self.get_widget_for_response(Gtk.ResponseType.CANCEL)
         ok_button = self.get_widget_for_response(Gtk.ResponseType.OK)
+
+        ok_button.connect("clicked", lambda x: self.emit("test1", "test"))
+        ok_button.connect("clicked", lambda x: self.emit("test2", "test"))
+
+
+        ## https://python-gtk-3-tutorial.readthedocs.io/en/latest/objects.html?highlight=events#signals
+        # https://blog.digitaloctave.com/posts/python/gtk3/16-gtk3-custom-signals-example.html
 
         cancel_button.connect("clicked", Gtk.main_quit)
 
@@ -29,5 +74,10 @@ if __name__ == "__main__":
 
     win = NoZettelFoundDialog()
     win.connect("destroy", Gtk.main_quit)
+    print(GObject.signal_lookup("auaiuauaiue", win), GObject.signal_lookup("test1", win))
+
+    win.connect("test1", lambda x, y: print("uuuu"))
+
+    print(GObject.signal_lookup("auaiuauaiue", win), GObject.signal_lookup("test1", win))
     win.show_all()
     Gtk.main()
