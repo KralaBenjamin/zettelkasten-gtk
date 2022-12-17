83cc0525678e750f2751721983dabbb3cb88673b
Benjamin Krala <kralabenjamin@gmail.com>
2022-12-17 21:31:04 +0100

    pylint Änderungen weil Codequalität

--- src/Settings.py
+++ src/Settings.py
@@ -1,59 +1,95 @@
+"""
+Module handles Settings of the Application
+"""
+
 import json
 import os
+from os.path import join
+from os import makedirs
+import sys
 
 import gi
-
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-from os import makedirs
 
 
 
 class Settings:
+    """
+    Class to handle the Settings of the Application.
+    """
 
     def __init__(self):
         """
         Init Settings
         """
-        #todo: Path Klasse verwenden
-        self.location = f"{os.environ['HOME']}/.config/zettelkasten-gtk/settings.json"
-        self.location_dir = f"{os.environ['HOME']}/.config/zettelkasten-gtk/"
-        try:
-            self.settings_dict = json.load(open(self.location))
+        self.location = join(
+            os.environ['HOME'],
+            ".config/zettelkasten-gtk/settings.json"
+        )
+        self.location_dir = join(
+            os.environ['HOME'],
+            ".config/zettelkasten-gtk/"
+        )
 
-        except:
+        try:
+            with open(self.location, "r", encoding="utf-8") as file:
+                self.settings_dict = json.load(file)
+        except Exception():
             self.settings_dict = dict()
             self.settings_dict["zk_locations"] = list()
             self.settings_dict["main_window"] = dict()
             self.settings_dict["zettel_window"] = dict()
 
     def is_settings_file_avalaible(self):
-        return bool(self.settings_dict) 
+        """
+        checks, if there is a setting file.
+        """
+        return bool(self.settings_dict)
 
     def save_settings(self):
+        """
+        save current settings.
+        """
         makedirs(self.location_dir)
-        json.dump(self.settings_dict, open(self.location, "w+"))
+        with open(self.location, "w+", encoding="utf-8") as file:
+            json.dump(self.settings_dict, file)
 
     def is_settings_zk_location_in_settings_file(self):
+        """
+        checks, if there is any zettelkasten in Settings.
+        """
         return "zk_locations" in self.settings_dict and \
             isinstance(self.settings_dict["zk_locations"], list) and \
             len(self.settings_dict["zk_locations"]) > 0
 
     def get_zk_locations(self):
+        """
+        returns all zettelkasten paths in Settings.
+        """
         return self.settings_dict["zk_locations"]
 
-    def set_zk_locations(self, zk_locations):
+    def set_zk_locations(self, zk_locations: list):
+        """
+        sets all zettelkasten locations.
+        """
         self.settings_dict["zk_locations"] = zk_locations
         self.save_settings()
 
-    def add_zk_location(self, new_zk_location):
+    def add_zk_location(self, new_zk_location: str):
+        """
+        adds a zettelkasten location
+        """
         self.settings_dict["zk_locations"].append(new_zk_location)
 
 
-def get_zettelkasten_location_dialog():        
+def get_zettelkasten_location_dialog():
+    """
+    Function opens a new Dialog so User can add first Zettelkasten Location.
+    """
 
     msg = Gtk.MessageDialog(
-        title="Kein Ordner mit Zettel gefunden", 
+        title="Kein Ordner mit Zettel gefunden",
         flags=0
     )
     msg.add_buttons(
@@ -65,7 +101,7 @@ def get_zettelkasten_location_dialog():
     msg_result = msg.run()
 
     if msg_result == -6: #cancel
-        exit()
+        sys.exit(0)
 
     msg.destroy()
 
@@ -79,21 +115,7 @@ def get_zettelkasten_location_dialog():
 
     response_file_chooser = file_chooser.run()
     if response_file_chooser == -6: #cancel
-        exit()
+        sys.exit(0)
     uri = file_chooser.get_uri()
 
     return uri.replace('file://', "")
-
-if __name__ == "__main__":
-    LOCATION = f"{os.environ['HOME']}/.config/zettelkasten-gtk/settings.json" 
-    seti = Settings()
-    print(seti.is_settings_zk_location_avalaible())
-    json.dump({}, open(LOCATION, "w+"))
-    print(seti.is_settings_zk_location_avalaible())
-    json.dump({"zk_locations": "a"}, open(LOCATION, "w+"))
-    print(seti.is_settings_zk_location_avalaible())
-    json.dump({"zk_locations": []}, open(LOCATION, "w+"))
-    print(seti.is_settings_zk_location_avalaible())
-    json.dump({"zk_locations": ["a"]}, open(LOCATION, "w+"))
-    print(seti.is_settings_zk_location_avalaible())
-    print(seti.get_zk_locations())
